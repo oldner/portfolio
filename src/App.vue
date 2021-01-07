004fbdcddf612228f6ad71ecaca40bd2dabde763
oldner <haktangs106@gmail.com>
2021-01-07 18:05:26 +0300

    added title - created script file

--- src/App.vue
+++ src/App.vue
@@ -194,51 +194,7 @@
   </div>
 </template>
 
-<script>
-import { onMounted } from "vue";
-export default {
-  name: "App",
-  data() {
-    return {
-      loader: false,
-      sideBar: false,
-      name: "",
-      content: "",
-      comments: [],
-      johnComment: true,
-    };
-  },
-  methods: {
-    addComment() {
-      var today = new Date();
-      var dd = String(today.getDate()).padStart(2, "0");
-      var mm = String(today.getMonth() + 1).padStart(2, "0");
-      var yyyy = today.getFullYear();
-
-      today = mm + "/" + dd + "/" + yyyy;
-      this.comments.push({
-        name: this.name,
-        content: this.content,
-        time: today,
-      });
-    },
-    removeComment(index) {
-      if (!index) {
-        this.johnComment = false;
-      }
-      this.comments.splice(index, 1);
-    },
-  },
-  beforeMount() {
-    this.loader = true;
-    setTimeout(
-      function () {
-        this.loader = false;
-      }.bind(this),
-      1000
-    );
-  },
-};
+<script src="./script.js">
 </script>
 
 <style src="./style.css">
---
+++ src/script.js
@@ -0,0 +1,48 @@
+import { onMounted } from "vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      loader: false,
+      sideBar: false,
+      name: "",
+      content: "",
+      comments: [],
+      johnComment: true,
+    };
+  },
+  methods: {
+    addComment() {
+      var today = new Date();
+      var dd = String(today.getDate()).padStart(2, "0");
+      var mm = String(today.getMonth() + 1).padStart(2, "0");
+      var yyyy = today.getFullYear();
+
+      today = mm + "/" + dd + "/" + yyyy;
+      this.comments.push({
+        name: this.name,
+        content: this.content,
+        time: today,
+      });
+      window.location.href = "#comments";
+    },
+    removeComment(index) {
+      if (!index) {
+        this.johnComment = false;
+      }
+      this.comments.splice(index, 1);
+    },
+  },
+  beforeMount() {
+    this.loader = true;
+    setTimeout(
+      function () {
+        this.loader = false;
+      }.bind(this),
+      1000
+    );
+  },
+  created() {
+    document.title = "Haktan Sefa Pasin | Jr. Web Developer";
+  },
+};
